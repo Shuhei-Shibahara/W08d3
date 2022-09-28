6a597629cab21254bc9113f279d4de3e1c6b59c8
shihyulung <shihyulung@yahoo.com.tw>
2022-09-28 14:47:34 -0700

    try to parse time

--- call_back_exercise.js
+++ call_back_exercise.js
@@ -9,18 +9,32 @@ class Clock {
     this.minutes = date.getMinutes();
     this.seconds = date.getSeconds();
     // this.printTime();
-    setInterval(this._tick, 1000);
+    setInterval(this._tick.bind(this), 0);
   }
 
   printTime() {
     // Format the time in HH:MM:SS
     // Use console.log to print it.
-    console.log( `${this.hours}:${this.minutes}:${this.seconds}`);
+    // if (this.seconds < 10){
+    //     this.seconds = `0${this.seconds}`
+    // }
+    // debugger
+    // if (this.minutes < 10 && this.minutes.length < 2){
+    //     this.minutes = `0${this.minutes}`
+    // }
+    // if (this.hours < 10){
+    //     this.hours = `0${this.hours}`
+    // }
+    const zeroPad = (num, places) => String(num).padStart(places, '0')
+    console.log(`${zeroPad(this.hours,2)}:${zeroPad(this.minutes)}:${zeroPad(this.seconds)}`);
   }
   _tick() {
     // 1. Increment the time by one second.
     // 2. Call printTime.
-    this.seconds++;
+    ++this.seconds;
+    if (this.seconds >= 60){ this.seconds = 0; this.minutes++;};
+    if (this.minutes >= 60){ this.minutes = 0; this.hours++};
+    if (this.hours >= 24){ this.hours = 0;};
     this.printTime();
   }
 }
