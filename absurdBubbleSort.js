e5bfbb9db39b68e32b5765eaae0fa7376cbc7b01
Shuhei Shibahara <Shoehey06@gmail.com>
2022-09-28 16:39:55 -0700

    created innerbubblesort

--- absurdBubbleSort.js
+++ absurdBubbleSort.js
@@ -6,20 +6,37 @@ const reader = readline.createInterface({
 
 
 const askIfGreaterThan = function(el1,el2,callback){
-    reader.question(`Is ${el1} greater than ${el2}?`, response=>{
+    reader.question(`Is ${el1} greater than ${el2}?\n`, response=>{
         if (response === "yes"){
             callback(true);
         } else {
             callback(false);
         }
-        reader.close();
+        // reader.close();
     })
 }
 
-console.log(askIfGreaterThan(1,2,function(bool){
-    if (bool){
-        console.log("cool")
-    } else {
-        console.log("not cool")
+const innerBubbleSortLoop = (arr, i, madeAnySwaps, outerBubbleSortLoop) => {
+    if (i < arr.length - 1) {
+        askIfGreaterThan(arr[i], arr[i+1], function(isGreaterThan){
+            if (isGreaterThan){
+                [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+                innerBubbleSortLoop(arr, i + 1, true, outerBubbleSortLoop); 
+            }else{
+                innerBubbleSortLoop(arr, i + 1, false, outerBubbleSortLoop); 
+            }
+        })
     }
-}));
+    else if (i == arr.length-1){
+        reader.close();
+        outerBubbleSortLoop(madeAnySwaps);
+        return arr
+    }
+}
+
+const arr = [1, 2, 5, 4]
+const sort = innerBubbleSortLoop(arr, 0, false, function(){console.log('in outter bubble sort')});
+// console.log(sort)
+
+
+
